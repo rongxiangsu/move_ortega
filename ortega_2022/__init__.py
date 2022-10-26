d5290bd0d73064e4383453e4afb37da19b2c3813
Rongxiang Su <rongxiangsu@gmail.com>
2022-10-26 11:14:42 -0700

    ignore some files

--- ortega_2022/__init__.py
+++ ortega_2022/__init__.py
@@ -1,6 +1,6 @@
-# from ortega_2022.ellipses import *
-# from ortega_2022.STPoint import *
-# from ortega_2022.ortega import *
+from ortega_2022.ellipses import *
+from ortega_2022.STPoint import *
+from ortega_2022.ortega import *
 
 # __title__ = "ortega_2022"
 # __version__ = "0.0.5"
