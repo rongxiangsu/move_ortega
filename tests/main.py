e5611e37089087880f22b9d229190c86c49e3858
Rongxiang Su <rongxiangsu@gmail.com>
2022-10-26 11:11:29 -0700

    Delete tests directory

--- tests/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import ortega_2022.ortega
-
-samplein = 10
-bigdata = pd.read_csv('/Users/rongxiangsu/rongxiang_jupyter/_data/CHTS_interaction/GpsPoints_'+str(samplein)+'min.csv',sep=',', header=0)
-bigdata = bigdata[(bigdata['pid']==298473101)|(bigdata['pid']==719939301)]
-
-print(bigdata.head())
-
